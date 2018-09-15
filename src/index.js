48f8b9e7844658c5cb71cb45ae3d873e16697103
TanyaSamal <solesytto@gmail.com>
2018-09-15 12:01:41 +0300

    resolve Money Exchange task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { debug } from "util";
-
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
  let coins = {};
  let halfDollars = "H";
@@ -13,22 +10,24 @@ module.exports = function makeExchange(currency) {
      return coins;
  }
 
- if (currency >= 10000){
+ if (currency > 10000){
     return {error: "You are rich, my friend! We don't have so much coins for exchange"};
  }
 
- let half = Math.floor(currency / 50);
- if (half > 0){
-    coins[halfDollars] = half;
-    let rest = (currency % 50) / 25;
+    let half = Math.floor(currency / 50);
+    if (half > 0) coins[halfDollars] = half;
+
+    let rest = Math.floor((currency % 50) / 25);
     if (rest > 0) coins[quarters] = rest;
-    rest = (rest % 25) / 10;
+
+    rest = Math.floor((currency % 50) % 25 / 10);
     if (rest > 0) coins[dimes] = rest;
-    rest = (rest % 10) / 5;
-    if (rest > 0) coins[nickels] = rest;
-    rest = (rest % 5);
-    if (rest > 0) coins[nickels] = rest;
- } 
 
+    rest = Math.floor((currency % 50) % 25 % 10 / 5);
+    if (rest > 0) coins[nickels] = rest;
 
+    rest = Math.floor((currency % 50) % 25 % 10 % 5);
+    if (rest > 0) coins[pennies] = rest;
+
+    return coins;
 }
