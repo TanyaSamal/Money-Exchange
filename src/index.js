4296c723d037142228698edf5ef9b920d51061c2
TanyaSamal <solesytto@gmail.com>
2018-09-15 11:26:55 +0300

    add file package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
+import { debug } from "util";
+
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+ let coins = {};
+ let halfDollars = "H";
+ let quarters = "Q";
+ let dimes = "D";
+ let nickels = "N";
+ let pennies = "P";
+
+ if (currency <= 0){
+     return coins;
+ }
+
+ if (currency >= 10000){
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+ }
+
+ let half = Math.floor(currency / 50);
+ if (half > 0){
+    coins[halfDollars] = half;
+    let rest = (currency % 50) / 25;
+    if (rest > 0) coins[quarters] = rest;
+    rest = (rest % 25) / 10;
+    if (rest > 0) coins[dimes] = rest;
+    rest = (rest % 10) / 5;
+    if (rest > 0) coins[nickels] = rest;
+    rest = (rest % 5);
+    if (rest > 0) coins[nickels] = rest;
+ } 
+
+
 }
